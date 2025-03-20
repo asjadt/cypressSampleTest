84e69e7ab425622020a72b1a593680dd2d491d2a
asjadtariq <asjadtariq@gmail.com>
2025-03-20 17:18:58 +0000

    Update HTML Report
    Update HTML Report

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Add custom event listeners here if needed
+      return config;
     },
+  },
+  reporter: "mochawesome", // Specify the reporter
+  reporterOptions: {
+    reportDir: "cypress/reports", // Directory where the report will be saved
+    overwrite: false, // Don't overwrite previous reports
+    html: true, // Generate an HTML report
+    json: true, // Generate a JSON report
   },
 });
