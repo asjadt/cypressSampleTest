b1ef3d864d3ca83f651db028e02df350b7c0bc88
asjadtariq <asjadtariq@gmail.com>
2025-03-23 04:56:46 +0000

    updated report

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,27 @@
 const { defineConfig } = require("cypress");
+const mochawesome = require("cypress-mochawesome-reporter/plugin");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Add custom event listeners here if needed
+      mochawesome(on); // Set up Mochawesome plugin
       return config;
     },
-  },
-  reporter: "mochawesome", // Specify the reporter
-  reporterOptions: {
-    reportDir: "cypress/reports", // Directory where the report will be saved
-    overwrite: false, // Don't overwrite previous reports
-    html: true, // Generate an HTML report
-    json: true, // Generate a JSON report
+    reporter: "cypress-mochawesome-reporter", // Specify the Mochawesome reporter
+    reporterOptions: {
+      reportDir: "cypress/reports", // Path where the report will be generated
+      reportFilename: "mochawesome", // The base filename for the report
+      charts: true, // Optional: Enable charts
+      reportPageTitle: "Test Report", // Optional: Title of the HTML report
+      embeddedScreenshots: true, // Enable embedding screenshots in the report
+      inlineAssets: true, // Inline CSS and JS assets
+      saveHtml: true, // Save the report as an HTML file
+      debug: true,
+    },
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    video: false,
+    screenshotsFolder: "cypress/screenshots", // Path for screenshots
+    screenshotOnRunFailure: true, // Automatically capture screenshots on failure
   },
 });
